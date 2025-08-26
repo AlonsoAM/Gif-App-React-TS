1df6b2eae81b17d89c0c4e86c9ed34f15685379b
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-08-26 18:49:37 -0500

    ✨ feat: agrega componente principal y estilos a la app
    - Creado el componente principal `GifApp` con estructura base.
    - Agregados estilos globales en `index.css` (configuración de fuente y colores).
    - Añadida integración con Google Fonts para usar "Montserrat Alternates".
    - Generados componentes para buscador, búsquedas previas y grilla de GIFs.
    - Incluido archivo `mock-data/gifs.mock.ts` con datos simulados para pruebas.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {GifApp} from "./GifApp.tsx";
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hola mundo</h1>
+    <GifApp/>
   </StrictMode>,
 )
